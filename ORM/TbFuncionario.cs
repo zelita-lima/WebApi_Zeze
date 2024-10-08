0e98564fe609163cd2f2e5ac9731ee649b035fe1
zelita-lima <163172165+zelita-lima@users.noreply.github.com>
2024-10-08 17:31:35 -0300

    Up01

---
+++ ORM/TbFuncionario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi_Zeze.ORM;
+
+public partial class TbFuncionario
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public int Idade { get; set; }
+
+    public byte[]? Foto { get; set; }
+}
---
+++ ORM/TbUsuarioTarde.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi_Zeze.ORM;
+
+public partial class TbUsuarioTarde
+{
+    public int Id { get; set; }
+
+    public string Usuario { get; set; } = null!;
+
+    public string Senha { get; set; } = null!;
+}
